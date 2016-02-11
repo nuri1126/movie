a86ac0d61452475524a48dae3c46bb1b30bcb5ee
nuri1126 <nury7230@naver.com>
2016-02-11 22:18:13 +0900

    parsing

---
+++ hw/parsing.py
@@ -0,0 +1,22 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw.settings')
+
+import django
+django.setup()
+
+from movie.models import Movie
+import urllib.request
+from bs4 import BeautifulSoup
+
+html = urllib.request.urlopen('http://www.google.co.kr/movies')
+soup = BeautifulSoup(html)
+title_tags = soup.find_all("div","name","a")
+
+
+for title_tag in title_tags:
+   print (title_tag)
+   title = title_tag.text
+   movie, is_created = Movie.objects.get_or_create(title=title)
+   movie.save()
+
+print('total movie : {}'.format(Movie.objects.all().count()))
